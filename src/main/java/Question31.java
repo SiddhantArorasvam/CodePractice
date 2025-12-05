e706ea35176c16f56517609e9c39da9ab0bc5076
Siddhant Arora <siddhantarora@Siddhants-MacBook-Air-2.local>
2025-12-05 14:34:00 +0530

    New Challenge

---
+++ src/main/java/Question31.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Question31 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 0, 2, 0, 0, 4, 5, 6, 7, 0};
+
+        System.out.println(Arrays.toString(Shuffle(arr)));
+
+    }
+
+    public static int[] Shuffle(int[] arr){
+
+
+
+        int index = 0;
+
+        for (int num :arr){
+
+            if (num !=0){
+
+                arr[index] = num;
+                index++;
+            }
+
+
+
+        }
+        while (index < arr.length){
+
+            arr[index] =0;
+            index++;
+        }
+
+        return arr;
+    }
+
+
+}
